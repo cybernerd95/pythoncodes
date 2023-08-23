d23b783d8303ae4ffffc01e56e2f2e605b50a47c
krishnaveni malladi <rohitmalladi45@gmail.com>
2023-08-23 22:03:14 +0530

    day 6 [and minor edits]

--- lcm_of_given_numbers.py
+++ lcm_of_given_numbers.py
@@ -1,11 +1,9 @@
 a = int(input('Enter a  '))
 b = int(input('Enter b  '))
-
 if a > b:
     c = a
 else:
-    c= b
-    
+    c= b  
 while True:
     if c % a == 0 and c  % b == 0:
         print(c)
---
+++ no_digits_in_number.py
@@ -0,0 +1,22 @@
+#Using lists 
+a= int(input("enter the number  "))
+b= int(input ("enter power of the number  "))
+c=a**b
+l=[]
+for n in str(c):
+   l.append(n)
+   print(len(l)) 
+print("Are the number of digits in a^b" ,a,b )
+print(c)
+
+
+
+# #Alternate method using logarithm
+# import math
+# a= int(input("enter the number  "))
+# b= int(input ("enter power of the number  "))
+# p = b*math.log10(a)
+# print(a, "power", b , " has ", int(p) +1 , "digits")
+
+#Another method would be continuously dividing it by 10 with giving a predefined i=0 and then adding +1 for 
+#for everytime the number is divided i=i+1 and until the number is less than 1 and then printni 
