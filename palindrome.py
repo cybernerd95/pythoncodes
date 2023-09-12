e0ea0a1b79b1ec9193c63b0beb40d87c81aba53c
krishnaveni malladi <rohitmalladi45@gmail.com>
2023-09-12 18:12:41 +0530

    check

--- palindrome.py
+++ palindrome.py
@@ -9,3 +9,7 @@ if e==c:
     print("it is a palindrome ")
 else:
     print("its not a palindrome ")
+
+    #the basic algoritm behind the code is adding each letter in the word in their decimal form into a list
+    #Then checking if thr reverse of the list and the original list are both same 
+    
